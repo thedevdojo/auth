ea6d10c6e2a75d4c8c48ebe923ab002b30157a36
Tony Lea <tnylea@gmail.com>
2024-04-17 15:02:32 -0400

    Adding latest updates to the auth installer

---
+++ src/Installer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Devdojo\Auth;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Artisan;
+
+class Installer extends ServiceProvider
+{
+    public function postInstall(){
+        Artisan::call('vendor:publish --tag=auth::assets');
+        Artisan::call('vendor:publish --tag=auth::config');
+    }
+
+    public function postUpdate(){
+        Artisan::call('vendor:publish --tag=auth::assets');
+    }
+}

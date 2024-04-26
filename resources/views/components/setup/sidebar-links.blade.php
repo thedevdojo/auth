b5571f4f07fb27fccde7de3e66bf2ee9393f6664
Tony Lea <tnylea@gmail.com>
2024-04-26 12:49:28 -0400

    Adding latest updates

--- resources/views/components/setup/sidebar-links.blade.php
+++ resources/views/components/setup/sidebar-links.blade.php
@@ -1,42 +1,32 @@
 <nav class="px-5 mt-3">
     <ul role="list" class="space-y-3">
+
+        <x-auth::setup.sidebar-link-item
+                    pageLink="welcome"
+                    icon="house"
+                    text="Welcome"
+                    class="-mx-2"
+                    :$page
+                ></x-auth::setup.sidebar-link-item>
         <li>
             <div class="text-xs font-semibold leading-6 text-gray-400">Configure</div>
             <ul role="list" class="-mx-2 mt-2 space-y-1">
                 <x-auth::setup.sidebar-link-item
-                    pageLink="login"
-                    icon="key"
-                    text="Login"
+                    pageLink="branding"
+                    icon="swatches"
+                    text="Customizations"
                     :$page
                 ></x-auth::setup.sidebar-link-item>
                 <x-auth::setup.sidebar-link-item
-                    pageLink="register"
+                    pageLink="providers"
                     icon="user-circle-plus"
-                    text="Register"
+                    text="Social Providers"
                     :$page
                 ></x-auth::setup.sidebar-link-item>
                 <x-auth::setup.sidebar-link-item
-                    pageLink="verify"
-                    icon="shield-check"
-                    text="Verify"
-                    :$page
-                ></x-auth::setup.sidebar-link-item>
-                <x-auth::setup.sidebar-link-item
-                    pageLink="password-request"
-                    icon="cursor-text"
-                    text="Password Reset Request"
-                    :$page
-                ></x-auth::setup.sidebar-link-item>
-                <x-auth::setup.sidebar-link-item
-                    pageLink="password-reset"
-                    icon="password"
-                    text="Password Reset"
-                    :$page
-                ></x-auth::setup.sidebar-link-item>
-                <x-auth::setup.sidebar-link-item
-                    pageLink="password-confirmation"
-                    icon="lock-key"
-                    text="Password Confirmation"
+                    pageLink="settings"
+                    icon="gear"
+                    text="Settings"
                     :$page
                 ></x-auth::setup.sidebar-link-item>
             </ul>
@@ -44,12 +34,6 @@
         <li>
             <div class="text-xs font-semibold leading-6 text-gray-400">Customize</div>
             <ul role="list" class="-mx-2 mt-2 space-y-1">
-                <x-auth::setup.sidebar-link-item
-                    pageLink="branding"
-                    icon="swatches"
-                    text="Branding"
-                    :$page
-                ></x-auth::setup.sidebar-link-item>
                 <x-auth::setup.sidebar-link-item
                     pageLink="appearance"
                     icon="paint-bucket"

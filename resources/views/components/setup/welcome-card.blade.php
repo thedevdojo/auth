6fb63a49793af8dd41bc50ae27fb7880b5a6a07e
Tony Lea <tnylea@gmail.com>
2024-04-27 21:04:54 -0400

    Adding more to auth

--- resources/views/components/setup/welcome-card.blade.php
+++ resources/views/components/setup/welcome-card.blade.php
@@ -1,10 +1,10 @@
-<button class="flex justify-start items-start p-8 bg-white rounded-lg border duration-300 ease-out text-zinc-500 hover:text-zinc-700 hover:bg-zinc-50 border-zinc-200 hover:border-zinc-300 group">
-    <div class="flex-shrink-0 w-20 h-20">
+<a href="{{ url($link) }}" wire:navigate class="flex justify-start items-start p-8 bg-white rounded-lg border duration-300 ease-out text-zinc-500 hover:text-zinc-700 hover:bg-zinc-50 border-zinc-200 hover:border-zinc-300 group">
+    <span class="flex-shrink-0 w-20 h-20">
         @include('auth::includes.setup.icons.' . ($icon ?? 'social-providers'))
-    </div>
+    </span>
     
-    <div class="flex flex-col items-start ml-5">
-        <h3 class="mb-2 text-lg font-semibold text-zinc-700">{{ $title ?? '' }}</h3>
-        <p class="mb-4 text-sm text-left">{{ $description ?? '' }}</p>
-    </div>
-</button>
+    <span class="flex flex-col items-start ml-5">
+        <span class="mb-2 text-lg font-semibold text-zinc-700">{{ $title ?? '' }}</span>
+        <span class="mb-4 text-sm text-left">{{ $description ?? '' }}</span>
+    </span>
+</a>
--- resources/views/pages/auth/setup/settings.blade.php
+++ resources/views/pages/auth/setup/settings.blade.php
@@ -16,8 +16,12 @@ new class extends Component
 <x-auth::layouts.setup>
 
     @volt('auth.setup.settings')
-        <section class="px-4 py-14 mx-auto w-full max-w-screen-lg">
+        <section class="px-4 py-5 mx-auto w-full max-w-screen-lg">
             <div class="mb-10">
+                <a href="/auth/setup" class="inline-flex items-center px-4 py-1.5 mb-3 space-x-1 text-sm font-medium rounded-full group bg-zinc-100 text-zinc-600 hover:text-zinc-800">
+                    <x-phosphor-arrow-left-bold class="w-3 h-3 duration-300 ease-out translate-x-0 group-hover:-translate-x-0.5" />
+                    <span>Back</span>
+                </a>
                 <h2 class="mb-2 text-2xl font-bold text-left">Settings</h2>
                 <p class="text-sm text-left text-gray-600">Adjust specific authentication features and enable/disable functionality.</p>
             </div>

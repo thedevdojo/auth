256b6cc5274c2ee7bc18a6a6ead6ef5ea8318785
Tony Lea <tnylea@gmail.com>
2024-06-12 11:10:34 -0400

    trying new dusk test case

--- tests/DuskTestCase.php
+++ tests/DuskTestCase.php
@@ -66,4 +66,17 @@ abstract class DuskTestCase extends BaseTestCase
             )
         );
     }
+
+    protected function captureFailuresFor($browsers)
+    {
+        $browsers->each(function (Browser $browser, $key) {
+          $body = $browser->driver->findElement(WebDriverBy::tagName('body'));
+            if (!empty($body)) {
+                $currentSize = $body->getSize();
+                $size = new WebDriverDimension($currentSize->getWidth(), $currentSize->getHeight());
+                $browser->driver->manage()->window()->setSize($size);
+            }
+            $browser->screenshot('failure-'.$this->getName().'-'.$key);
+        });
+    }
 }

28afd9def39094a5c7a993b8dfb263f40e8ff1f7
Tony Lea <tnylea@gmail.com>
2024-06-22 08:05:35 -0400

    fixing a bit of formatting

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -119,68 +119,68 @@ new class extends Component
 <x-auth::layouts.app title="{{ config('devdojo.auth.language.login.page_title') }}">
     <div class="relative w-full h-full">
         @volt('auth.login')
-        <div class="relative w-full">
-            <x-auth::elements.container>
+            <div class="relative w-full">
+                <x-auth::elements.container>
 
-                <x-auth::elements.heading
-                    :text="($language->login->headline ?? 'No Heading')"
-                    :description="($language->login->subheadline ?? 'No Description')"
-                    :show_subheadline="($language->login->show_subheadline ?? false)" />
+                    <x-auth::elements.heading
+                        :text="($language->login->headline ?? 'No Heading')"
+                        :description="($language->login->subheadline ?? 'No Description')"
+                        :show_subheadline="($language->login->show_subheadline ?? false)" />
 
-                @if(config('devdojo.auth.settings.login_show_social_providers') && config('devdojo.auth.settings.social_providers_location') == 'top')
-                    <x-auth::elements.social-providers />
-                @endif
+                    @if(config('devdojo.auth.settings.login_show_social_providers') && config('devdojo.auth.settings.social_providers_location') == 'top')
+                        <x-auth::elements.social-providers />
+                    @endif
 
-                <form wire:submit="authenticate" class="space-y-5">
+                    <form wire:submit="authenticate" class="space-y-5">
 
-                    @if($showPasswordField)
-                        <x-auth::elements.input-placeholder value="{{ $email }}">
-                            <button type="button" data-auth="edit-email-button" wire:click="editIdentity" class="font-medium text-blue-500">{{ config('devdojo.auth.language.login.edit') }}</button>
-                        </x-auth::elements.input-placeholder>
-                    @else
-                        @if($showIdentifierInput)
-                            <x-auth::elements.input :label="config('devdojo.auth.language.login.email_address')" type="email" wire:model="email" autofocus="true" data-auth="email-input" id="email" required />
+                        @if($showPasswordField)
+                            <x-auth::elements.input-placeholder value="{{ $email }}">
+                                <button type="button" data-auth="edit-email-button" wire:click="editIdentity" class="font-medium text-blue-500">{{ config('devdojo.auth.language.login.edit') }}</button>
+                            </x-auth::elements.input-placeholder>
+                        @else
+                            @if($showIdentifierInput)
+                                <x-auth::elements.input :label="config('devdojo.auth.language.login.email_address')" type="email" wire:model="email" autofocus="true" data-auth="email-input" id="email" required />
+                            @endif
                         @endif
-                    @endif
 
-                    @if($showSocialProviderInfo)
-                        <div class="p-4 text-sm rounded-md border bg-zinc-50 border-zinc-200">
-                            <span>You have been authenticated via {{ implode(', ', $userSocialProviders) }}. Please login to that network below.</span>
-                            <button wire:click="editIdentity" type="button" class="underline translate-x-1.5">Change Email</button>
-                        </div>
+                        @if($showSocialProviderInfo)
+                            <div class="p-4 text-sm rounded-md border bg-zinc-50 border-zinc-200">
+                                <span>You have been authenticated via {{ implode(', ', $userSocialProviders) }}. Please login to that network below.</span>
+                                <button wire:click="editIdentity" type="button" class="underline translate-x-1.5">Change Email</button>
+                            </div>
 
-                        @if(!config('devdojo.auth.settings.login_show_social_providers'))
-                            <x-auth::elements.social-providers
-                                :socialProviders="\Devdojo\Auth\Helper::getProvidersFromArray($userSocialProviders)"
-                                :separator="false"
-                            />
+                            @if(!config('devdojo.auth.settings.login_show_social_providers'))
+                                <x-auth::elements.social-providers
+                                    :socialProviders="\Devdojo\Auth\Helper::getProvidersFromArray($userSocialProviders)"
+                                    :separator="false"
+                                />
+                            @endif
                         @endif
+
+                        @if($showPasswordField)
+                            <x-auth::elements.input :label="config('devdojo.auth.language.login.password')" type="password" wire:model="password" id="password" data-auth="password-input" />
+                            <div class="flex justify-between items-center mt-6 text-sm leading-5">
+                                <x-auth::elements.text-link href="{{ route('auth.password.request') }}" data-auth="forgot-password-link">{{ config('devdojo.auth.language.login.forget_password') }}</x-auth::elements.text-link>
+                            </div>
+                        @endif
+
+                        <x-auth::elements.button type="primary" data-auth="submit-button" rounded="md" size="md" submit="true">
+                            {{ config('devdojo.auth.language.login.button') }}
+                        </x-auth::elements.button>
+                    </form>
+
+
+                    <div class="mt-3 space-x-0.5 text-sm leading-5 text-left" style="color:{{ config('devdojo.auth.appearance.color.text') }}">
+                        <span class="opacity-[47%]"> {{ config('devdojo.auth.language.login.dont_have_an_account') }} </span>
+                        <x-auth::elements.text-link data-auth="register-link" href="{{ route('auth.register') }}">{{ config('devdojo.auth.language.login.sign_up') }}</x-auth::elements.text-link>
+                    </div>
+
+                    @if(config('devdojo.auth.settings.login_show_social_providers') && config('devdojo.auth.settings.social_providers_location') != 'top')
+                        <x-auth::elements.social-providers />
                     @endif
 
-                    @if($showPasswordField)
-                        <x-auth::elements.input :label="config('devdojo.auth.language.login.password')" type="password" wire:model="password" id="password" data-auth="password-input" />
-                        <div class="flex justify-between items-center mt-6 text-sm leading-5">
-                            <x-auth::elements.text-link href="{{ route('auth.password.request') }}" data-auth="forgot-password-link">{{ config('devdojo.auth.language.login.forget_password') }}</x-auth::elements.text-link>
-                        </div>
-                    @endif
-
-                    <x-auth::elements.button type="primary" data-auth="submit-button" rounded="md" size="md" submit="true">
-                        {{ config('devdojo.auth.language.login.button') }}
-                    </x-auth::elements.button>
-                </form>
-
-
-                <div class="mt-3 space-x-0.5 text-sm leading-5 text-left" style="color:{{ config('devdojo.auth.appearance.color.text') }}">
-                    <span class="opacity-[47%]"> {{ config('devdojo.auth.language.login.dont_have_an_account') }} </span>
-                    <x-auth::elements.text-link data-auth="register-link" href="{{ route('auth.register') }}">{{ config('devdojo.auth.language.login.sign_up') }}</x-auth::elements.text-link>
-                </div>
-
-                @if(config('devdojo.auth.settings.login_show_social_providers') && config('devdojo.auth.settings.social_providers_location') != 'top')
-                    <x-auth::elements.social-providers />
-                @endif
-
-            </x-auth::elements.container>
-        </div>
+                </x-auth::elements.container>
+            </div>
         @endvolt
     </div>
 </x-auth::layouts.app>

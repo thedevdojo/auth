65364fb32ccb29c2f32325d691b2bd8f172cf8f6
skdishansachin <skdishansachin@gmail.com>
2024-06-27 18:52:07 +0530

    Add password confirmation to configuration files

--- config/devdojo/auth/settings.php
+++ config/devdojo/auth/settings.php
@@ -7,6 +7,7 @@ return [
     'redirect_after_auth' => '/',
     'registration_show_password_same_screen' => true,
     'registration_include_name_field' => false,
+    'registration_include_password_confirmation_field' => false,
     'registration_require_email_verification' => false,
     'enable_branding' => true,
     'dev_mode' => false,

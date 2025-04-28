fbb8a07a57fb4abdf6e047a7d1ef72b8a9a3375e
Tony Lea <tnylea@gmail.com>
2025-04-28 19:26:16 -0400

    Adding fixes to logo click

--- resources/views/components/elements/logo.blade.php
+++ resources/views/components/elements/logo.blade.php
@@ -1,7 +1,7 @@
-<x-auth::elements.link href="/" style="height:{{ $height ?? '30' }}px; width:auto; display:block" aria-label="{{ config('app.name') }} Logo">
+<a href="/" style="height:{{ $height ?? '30' }}px; width:auto; display:block" aria-label="{{ config('app.name') }} Logo">
     @if($isImage)
         <img src="{{ url($imageSrc) }}" style="height:100%; width:auto" alt="" />
     @else
         {!! str_replace('<svg', '<svg style="height:100%; width:auto"', $svgString) !!}
     @endif
-</x-auth::elements.link>
+</a>
